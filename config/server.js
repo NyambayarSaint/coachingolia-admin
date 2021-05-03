513ea347ce0e38e043f9d9588034a08cfcebb36d
nyambayarsaint <nyambayar.saint@gmail.com>
2021-05-03 21:30:15 +0800

    save

--- config/server.js
+++ config/server.js
@@ -6,4 +6,7 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET', '78c9c6e50104630cd77591c0b3d16363'),
     },
   },
+  CLOUDINARY_NAME: 'yondosoft',
+  CLOUDINARY_KEY: '538488111317522',
+  CLOUDINARY_SECRET: 'L7pOvemB1Rtzjxd3Lc3gRjhEjWs'
 });
