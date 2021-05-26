475d9c1a9627d6957184d34eb4675cfd6bd4d19c
nyambayarsaint <nyambayar.saint@gmail.com>
2021-05-26 18:54:05 +0800

    fuck

--- api/registration/models/registration.js
+++ api/registration/models/registration.js
@@ -1,15 +1,3 @@
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: 'nyambayar.saint@gmail.com',
-        pass: 'Popersia1997n'
-    }
-});
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
@@ -17,24 +5,44 @@ const transporter = nodemailer.createTransport({
  */
 
 module.exports = {
-    lifecycles: {
-        async beforeCreate(data) {
-            console.log(data, 'lalalararawerawr');
-            try {
-                // TO SEND EMAIL
-                transporter.sendMail({
-                    from: 'nyambayar.saint@gmail.com',
-                    to: `shine.dagva@gmail.com, nyambayar.saint2@gmail.com, shine.dagva@coachingolia.com`,
-                    subject: data.Subject,
-                    html: 'Нэр: ' + data.Firstname + ', Овог:' + data.Lastname + ', И-мэйл:' + data.Email + ', Утас:' + data.Phone + ', Message: ' + data.Message
-                }, function (error, info) {
-                    if (error) throw new Error(error)
-                    else console.log(info.response)
-                });
-            }
-            catch (e) {
-                console.log(e, 'fail');
-            }
-        }
-    },
 };
+
+
+
+
+// const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+// const nodemailer = require('nodemailer');
+
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 465,
+//     secure: true,
+//     auth: {
+//         user: 'nyambayar.saint@gmail.com',
+//         pass: 'Popersia1997n'
+//     }
+// });
+
+
+// module.exports = {
+//     lifecycles: {
+//         async beforeCreate(data) {
+//             console.log(data, 'lalalararawerawr');
+//             try {
+//                 // TO SEND EMAIL
+//                 transporter.sendMail({
+//                     from: 'nyambayar.saint@gmail.com',
+//                     to: `shine.dagva@gmail.com, nyambayar.saint2@gmail.com, shine.dagva@coachingolia.com`,
+//                     subject: data.Subject,
+//                     html: 'Нэр: ' + data.Firstname + ', Овог:' + data.Lastname + ', И-мэйл:' + data.Email + ', Утас:' + data.Phone + ', Message: ' + data.Message
+//                 }, function (error, info) {
+//                     if (error) throw new Error(error)
+//                     else console.log(info.response)
+//                 });
+//             }
+//             catch (e) {
+//                 console.log(e, 'fail');
+//             }
+//         }
+//     },
+// };
